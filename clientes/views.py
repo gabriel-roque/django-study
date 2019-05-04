52c6d9aa1626355d4bcc32a8f31c606403014749
gabriel-roque <gabrielrms.dll@gmail.com>
2019-05-04 18:17:45 -0300

    [UPDATE] limpando codigo

--- clientes/views.py
+++ clientes/views.py
@@ -25,10 +25,9 @@ def atualizarCliente(request, id):
 
     # instancia o form e retorna o cliente e separa o registro
     form = ClienteForm(request.POST or None, request.FILES or None, instance=cliente)
-    form2 = ClienteForm(request.POST, request.FILES, instance=cliente)
 
-    if form2.is_valid():
-        form2.save()
+    if form.is_valid():
+        form.save()
         return redirect('listar.cliente')
 
     return render(request, 'update_cliente.html', {'form': form, 'cliente': cliente})
