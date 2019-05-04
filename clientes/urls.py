8334d16b54dfffa0fbeec7f82ec317b76576ef10
gabriel-roque <gabrielrms.dll@gmail.com>
2019-05-04 18:04:25 -0300

    [UPDATE] Atualizando cliente somente por {{ form }}

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', listarClientes, name="listar.cliente"),
-    path('cliente/novo/', armazenarCliente, name="novo.cliente")
+    path('cliente/novo/', armazenarCliente, name="novo.cliente"),
+    path('cliente/up/<int:id>', atualizarCliente, name="update.cliente"),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cliente
 from .forms import ClienteForm
 
@@ -18,3 +17,19 @@ def armazenarCliente(request):
         return redirect('listar.cliente')
 
     return render(request, 'novo_cliente.html', {'form': form})
+
+
+def atualizarCliente(request, id):
+    # Se nao achar o objeto retora um 404
+    cliente = get_object_or_404(Cliente, pk=id)
+
+    # instancia o form e retorna o cliente e separa o registro
+    form = ClienteForm(request.POST or None, request.FILES or None, instance=cliente)
+    form2 = ClienteForm(request.POST, request.FILES, instance=cliente)
+
+    if form2.is_valid():
+        form2.save()
+        return redirect('listar.cliente')
+
+    return render(request, 'update_cliente.html', {'form': form, 'cliente': cliente})
+
